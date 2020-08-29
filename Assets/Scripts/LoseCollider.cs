23de1d2ebe32bec9bea8d1e695c362ca8b629595
Aleks Popov <popovaleks444@gmail.com>
2020-08-29 12:45:50 +0300

    Messed around with files. No meaningful changes.

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LoseCollider : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+
+        }
+    }
+}
